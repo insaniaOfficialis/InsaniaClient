270e9c35740cbbe45ed5878792481bfab6b5c734
insaniaOfficialis <insania_officialis@vk.com>
2023-09-09 19:32:06 +0300

    Добавлена привязка ролей по запросу с бека на окне регистрации

--- Client/Controls/Administrators/Registration.xaml.cs
+++ Client/Controls/Administrators/Registration.xaml.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Win32;
+﻿using Client.Models.Base;
+using Microsoft.Win32;
 using System;
-using System.IO;
+using System.Configuration;
+using System.Net.Http;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
@@ -12,9 +15,37 @@ namespace Client.Controls.Administrators;
 /// </summary>
 public partial class Registration : UserControl
 {
+    JsonSerializerOptions _settings = new();
     public Registration()
     {
         InitializeComponent();
+
+        _settings.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+
+        GetRoles();
+    }
+
+    public async void GetRoles()
+    {
+        string path = null;
+
+        if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings["DefaultConnection"])
+            && !string.IsNullOrEmpty(ConfigurationManager.AppSettings["Api"])
+            && !string.IsNullOrEmpty(ConfigurationManager.AppSettings["Roles"]))
+        {
+            path = ConfigurationManager.AppSettings["DefaultConnection"] + ConfigurationManager.AppSettings["Api"] + ConfigurationManager.AppSettings["Roles"] + "list";
+        }
+
+        using HttpClient client = new();
+        var result = await client.GetAsync(path);
+
+        if (result != null && result.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            var content = await result.Content.ReadAsStringAsync();
+            BaseResponseList response = JsonSerializer.Deserialize<BaseResponseList>(content, _settings);
+
+            Roles.ItemsSource = response.Items;
+        }
     }
 
     private void Image_Drop(object sender, DragEventArgs e)
@@ -31,8 +62,8 @@ public partial class Registration : UserControl
     private void ButtonLoadImage_Click(object sender, RoutedEventArgs e)
     {
         OpenFileDialog openFileDialog = new();
-        openFileDialog.Filter = "Image files (*.txt)|*.txt|All files (*.*)|*.*";
-        /*if (openFileDialog.ShowDialog() == true)
-            txtEditor.Text = File.ReadAllText(openFileDialog.FileName);*/
+        openFileDialog.Filter = "Image files (*.pdf;*.png;*.jpeg;*.jpg;*.bmp)|*.pdf;*.png;*.jpeg;*.jpg;*.bmp";
+        if (openFileDialog.ShowDialog() == true)
+            ImageLoad.Source = new BitmapImage(new Uri(openFileDialog.FileName));
     }
 }
--- Client/Properties/AssemblyInfo.cs
+++ Client/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("Insania")]
+[assembly: AssemblyTitle("Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Insania")]
+[assembly: AssemblyProduct("Client")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
